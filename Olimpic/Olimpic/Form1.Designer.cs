8f11e126685c6e8d5cebc0fab61a7a56c1a7f187
Krisztina Dózsa <krisztina.dozsa@stud.uni-corvinus.hu>
2021-11-30 23:58:08 +0100

    kész zh

--- Olimpic/Olimpic/Form1.Designer.cs
+++ Olimpic/Olimpic/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cbxev = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbxev
+            // 
+            this.cbxev.FormattingEnabled = true;
+            this.cbxev.Location = new System.Drawing.Point(83, 41);
+            this.cbxev.Name = "cbxev";
+            this.cbxev.Size = new System.Drawing.Size(121, 21);
+            this.cbxev.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Év";
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(16, 106);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(103, 23);
+            this.btnExcel.TabIndex = 2;
+            this.btnExcel.Text = "Excelbe ment";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxev);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbxev;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExcel;
     }
 }
 
