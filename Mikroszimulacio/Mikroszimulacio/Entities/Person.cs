e080928ec57f63e4c57b8dfb4836f86940eda358
Krisztina Dózsa <krisztina.dozsa@stud.uni-corvinus.hu>
2021-12-19 16:15:00 +0100

    kész

---
+++ Mikroszimulacio/Mikroszimulacio/Entities/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Person()
+        {
+            IsActive = true;
+        }
+
+
+    }
+}
